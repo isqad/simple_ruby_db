0346312e462970f288a94d487648534e26d4d7e3
Andrey N. Shalaev <andrey.shalaev@deeprosoft.com>
2013-09-09 19:26:06 +0600

    fixed search

--- db.rb
+++ db.rb
@@ -22,57 +22,27 @@ class Db
   # the condition can be a hash whose keys define the selection criteria
   # The key can contain conditions more or less for the search ranges in values
   # An example of the sample on the Range:
-  # db.search({"age.gt" => 20, "age.lt" => 25})
+  # db.search({:age => 20..40, :height => 150})
   def search(condition={})
     return @db if condition.nil? || !condition.is_a?(Hash) || condition.empty?
     keys = []
 
-    condition = parse condition
+    first_key, first_value = condition.first
 
-    condition.each do |key, value|
-      if value.is_a?(Array)
-        self.send("#{key}_index".to_sym).range(value[0], value[1]).each do |v|
-          keys |= v
-        end
-      else
-        keys |= self.send("#{key}_index".to_sym).get(value)
-      end
+    keys = if first_value.is_a?(Range)
+      send("#{first_key}_index").range(first_value.begin, first_value.end).reduce(:|)
+    else
+      send("#{first_key}_index").get(first_value)
     end
+
     result = Hash.new
     keys.each{|idx| result[idx] = @db[idx] }
+
+    condition.reject{|k,v| k == first_key}.each do |key, value|
+      result.select!{|idx, row| value.is_a?(Range) ? value.include?(row[key]) : row[key] == value }
+    end
+
     result
-  end
-
-  def valid_criteria(condition)
-    keys = %w(age amount height weight)
-
-    valid_keys = keys + keys.map{|v| v + '.gt'} + keys.map{|v| v + '.lt' }
-
-    condition = condition.keys.uniq & valid_keys
-    keys.each do |k|
-      condition.reject!{|v| v == k && (condition.include?("#{k}.lt") || condition.include?("#{k}.gt"))  }
-    end
-    condition
-  end
-
-  def parse(condition)
-    condition = condition.each_with_object({}){|(k,v), h| h[k.to_s] = v}
-
-    criteria = valid_criteria(condition)
-
-    ranges = {}
-
-    criteria.each do |v|
-      cond = v.split('.')
-      key = cond[0].to_sym
-      ranges[key] = [nil, nil] unless ranges.has_key?(key)
-      if cond[1] == 'gt'
-        ranges[key][0] = condition[v]
-      elsif cond[1] == 'lt'
-        ranges[key][1] = condition[v]
-      end
-    end
-    ranges.each_with_object({}){|(k,v), h| h[k] = (v == [nil, nil] ? condition[k.to_s] : v) }
   end
 
   def create
